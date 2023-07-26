dd5e87a9bc61fc3f167f9439c7dbd000b4b30436
Bartek <bartekszalowski59@gmail.com>
2023-07-26 14:29:47 +0200

    created navbar

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; 2023 BartlomiejSzalowskiStore, Inc. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
