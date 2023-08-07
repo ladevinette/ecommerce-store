7acb1488b6fa0473fec952941503ade43fd9d892
Bartek <bartekszalowski59@gmail.com>
2023-08-07 16:21:16 +0200

    created featuredProducts

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,26 @@
-const HomePage = () => {
-  return <div>HomePage</div>;
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("683e4828-5651-41f8-9739-9a1d5cafd825");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
